c1a8ea1ba4c151f7914c791e1da443b4185366db
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-02-11 20:43:23 +0300

    update project_e_shop

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 app_name = "catalog"
@@ -7,4 +6,6 @@ app_name = "catalog"
 urlpatterns = [
     path("", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
+    path('products_list/', views.products_list, name='products_list'),
+    path('product_detail/<int:product_id>/', views.product_detail, name='product_detail')
 ]
