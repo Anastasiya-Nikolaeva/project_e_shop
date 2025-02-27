7eb2b6fb4be224540aa4b963c0505e3a9877d10d
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-02-27 12:16:20 +0300

    update project_e_shop

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from . import views
+
+from .views import ContactsView, HomeView, ProductDetailView, ProductsListView, ProductDeleteView, ProductUpdateView, \
+    ProductCreateView
 
 app_name = "catalog"
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("contacts/", views.contacts, name="contacts"),
-    path('products_list/', views.products_list, name='products_list'),
-    path('product_detail/<int:product_id>/', views.product_detail, name='product_detail')
+    path("", HomeView.as_view(), name="home"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("products/", ProductsListView.as_view(), name="products_list"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+
+    path("products/create/", ProductCreateView.as_view(), name="product_create"),
+    path("products/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_edit"),
+    path("products/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,40 +1,77 @@
 from django.http import Http404
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import DetailView, ListView, TemplateView, CreateView, UpdateView, DeleteView
+
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
-def home(request):
-    latest_products = Product.objects.order_by('-created_at')[:5]
-    products = Product.objects.all()
+class HomeView(TemplateView):
+    template_name = "catalog/home.html"
 
-    return render(request, "catalog/home.html", {
-        "latest_products": latest_products,
-        "products": products,
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["latest_products"] = Product.objects.order_by("-created_at")[:5]
+        context["products"] = Product.objects.all()
+        return context
 
 
-def contacts(request):
-    message = None
-    if request.method == "POST":
+class ContactsView(View):
+    template_name = "./catalog/contacts.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {"message": None})
+
+    def post(self, request):
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message_content = request.POST.get("message")
 
         message = "Ваше сообщение успешно отправлено!"
-
-    return render(request, "./catalog/contacts.html", {"message": message})
-
-
-def products_list(request,):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/products_list.html', context)
+        return render(request, self.template_name, {"message": message})
 
 
-def product_detail(request, product_id):
-    try:
-        product = Product.objects.get(id=product_id)
-    except Product.DoesNotExist:
-        raise Http404("Продукт не найден")
-    context = {'product': product}
-    return render(request, 'catalog/product_detail.html', context)
+class ProductsListView(ListView):
+    model = Product
+    template_name = "catalog/products_list.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        return Product.objects.order_by('created_at')
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj is None:
+            raise Http404("Продукт не найден")
+        return obj
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy('catalog:products_list')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy('catalog:products_list')
