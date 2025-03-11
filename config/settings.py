859b931daf8537d3a8aab98f9b17ed8eea093669
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-03-12 02:05:29 +0300

    update project_e_shop

--- config/settings.py
+++ config/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eritreya666@gmail.com'
-EMAIL_HOST_PASSWORD = 'TmVx01-br'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
 
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = 'catalog:home'
