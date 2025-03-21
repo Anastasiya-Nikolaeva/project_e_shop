52058f28ac0f10d65574c23ae60eae5a21448ad9
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-03-21 18:59:12 +0300

    update project_e_shop

--- catalog/services.py
+++ catalog/services.py
@@ -1,5 +1,13 @@
+from django.core.cache import cache
 from .models import Product
 
 
 def get_products_by_category(category_id):
-    return Product.objects.filter(category_id=category_id)
+    cache_key = f'products_category_{category_id}'
+    products = cache.get(cache_key)
+
+    if products is None:
+        products = Product.objects.filter(category_id=category_id)
+        cache.set(cache_key, products, timeout=60*15)
+
+    return products
